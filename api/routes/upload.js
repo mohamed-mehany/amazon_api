05952f4c1f1ac15b8d75e751a1a3ae92c2a81eb0
Omar Essam <o.essameldin@gmail.com>
2017-05-03 01:16:45 +0200

    remove unneeded attributes

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -2,8 +2,6 @@ const router = express.Router();
 
 router.post('/', upload.single('avatar'), function(req, res, next) {
     console.log(req.file);
-    console.log(req.body.password);
-    console.log(req.body.name);
     return res.send({ success: 'image uploaded successfully' });
 })
 module.exports = router;
