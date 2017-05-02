809157d663ab583d4809e758a05ab4a6151a110f
Omar Essam <o.essameldin@gmail.com>
2017-05-02 23:56:54 +0200

    edit uplaod.js

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -1,6 +1,9 @@
 const router = express.Router();
 
 router.post('/', upload.single('avatar'), function(req, res, next) {
+    console.log(req.file);
+    console.log(req.body.password);
+    console.log(req.body.name);
     return res.send({ success: 'image uploaded successfully' });
 })
 module.exports = router;
